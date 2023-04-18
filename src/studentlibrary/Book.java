1bdf01aed53241239814ec2d1eb3713f4e9db965
felipe <felipe@10.250.30.17>
2023-04-18 11:38:32 +0100

    Update to Attributes, Getters and setters in Book Class

--- src/studentlibrary/Book.java
+++ src/studentlibrary/Book.java
@@ -9,10 +9,67 @@ package studentlibrary;
  * @author caio_
  */
 public class Book {
+
+   private String id, fName, lName, title, genre;
+
+    public Book(String id, String fName, String lName, String title, String genre) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lName;
+        this.title = title;
+        this.genre = genre;
+        
+    }
+
+    public Book() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", fName=" + fName + ", lName=" + lName + ", title=" + title + ", genre=" + genre + ", waitingList=" + '}';
+    }
     
-    String ID;
-    String Author_first_name;
-    String Author_second_name;
     
     
 }
